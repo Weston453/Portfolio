c5cc3de1ea2ce883669aa7d0f82ed2f6ffb54df7
Weston Isheim <wisheim@gmail.com>
2021-12-17 20:27:01 -0700

    added some content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const Contact = () => {
     return (
         <>
-            <section className="contact" id="contact">
+            <section id="contact">
+                <div className="contact">
                 <h1 className="section-title">Contact</h1>
+                </div>
+                <div id="base"></div>
             </section>
         </>
     )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Home.css';
+import arrow from './Images/arrow.svg'
 
 const Home = () => {
     return (
         <>
             <section className="home" name="home" id="home">
                 <h1>Hello, I'm Weston Isheim.</h1>
-                <h1>I'm a full-stack web developer</h1>
-                <a href='#projects'><button>Check me out!</button></a>
+                <h1>I'm a full-stack web developer.</h1>
+                <h1 className="work-bellow">Check out some of my work down bellow!</h1>
+                <a href='#projects'><img className="arrow" src={arrow} alt="view projects" /></a>
             </section>
         </>
     )
